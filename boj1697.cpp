6bca43a0ab91a3ab3cd421b93247b1581a616f63
dhchoi1457 <alvin1785@gmail.com>
2019-02-19 23:34:25 +0900

    [boj1697]_숨바꼭질_bfs

--- boj1697.cpp
+++ boj1697.cpp
@@ -1,13 +1,13 @@
+//boj 1697
+// 숨바꼭질
+
+// Q.
+// int dist[200001]; // 10만일떄 안되다가, 20만일떄 통과.. 왜지?
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int dx[3]={-1,1,0}; //이동 방향
-//여기서 dist = -1로 초기화하고 조건주면 문제 생김.
-// dist로 조건걸때 방문체크가  안되서 겹침.
-// 이전에 2차원일땐 이걸로 어떻게 했었는데.. 그건 왜됐지?
-//이렇게 해도 되는거 맞는데.. 뭐에서 틀리는거지?
-// 맞 왜 틀 ㅠㅜㅜ
-
 
 int main()
 {
@@ -17,37 +17,37 @@ int main()
 	int N, K;
 	cin >> N >> K;
 
-	int dist[100002];
-	fill(dist,dist+100000,-1); // fill쓰는거 연습하자
+	if(N>=K){
+		cout << N-K << '\n';
+		return 0;
+	}
+
+	int dist[200001]; // 10만일떄 안되다가, 20만일떄 통과.. 왜지?
+	fill(dist,dist+200000,-1); // fill쓰는거 연습하자
 	queue<int> Q;
 	Q.push(N);
 	dist[N]=0;
-	int n = 100;
 
-	while(n>0)
+
+	while(!Q.empty())
 	{
-		if(N>=K){
-			cout << N-K << '\n';
-			break;
-		}
 		int now = Q.front(); Q.pop();
 		if(now ==K)
 		{
-			cout << dist[now] << "정답은!"<<'\n';
+			cout << dist[now]<<'\n';
 			break;
 		}
-		cout << "now : "<< now << "now_dist:"<<dist[now]<<'\n';
+		//cout << "now : "<< now << "now_dist:"<<dist[now]<<'\n';
 		for(int dir=0; dir<3; dir++)
 		{
 			int next = now + dx[dir];
 			if(dir==2) next = 2*now; 
-			if(next<0 or next >=100000) continue;
+			if(next<0 or next >100000) continue; //범위 항상 잘 체크
 			if(dist[next]>=0) continue; // 시작 조건에 따라.. ㅠㅜ 이거 조심  > or >=.. 일단 이거 고쳤는데 안되네 
 			Q.push(next);
 			dist[next] = dist[now]+1;
-			cout << "next : "<< next <<"next_dist:"<<dist[next]<<'\n'; 
+		//	cout << "next : "<< next <<"next_dist:"<<dist[next]<<'\n'; 
 		}
-		n--;
 	}
 	return 0;
 }
