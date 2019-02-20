dfe88be5681898034586ae753c04726f84b01d82
dhchoi1457 <alvin1785@gmail.com>
2019-02-20 22:07:31 +0900

    [boj2206]벽부수고이동하기_BFS

--- boj2206.cpp
+++ boj2206.cpp
@@ -1,13 +1,16 @@
 // boj 2206
 // 벽부수고 이동
 // 무엇을 고치면 될까?
-// Not yet, 재도전
+// Not yet, 재도전 후 pass 
+// 1.continue하는 범위 수정
+// 2.인풋도 잘못되어있었음
+// 3.dist[N-1][M-1] = -1로 초기화해서 활용했던거 수정
 
 #include <bits/stdc++.h>
 using namespace std;
 
 #define X first
-#define Y second // 여기에 ;붙이면 에러남. 주의
+#define Y second
 
 struct pos
 {
@@ -28,7 +31,8 @@ int main()
 	cin.tie(0);
 
 	cin >> N >> M; // N= 6 M=4;
-	for(int i=0; i<M; i++)
+	
+	for(int i=0; i<N; i++)
 	{
 		cin >> board[i];
 	}
@@ -37,55 +41,52 @@ int main()
 	pos s;
 	s.x = 0; s.y=0; s.bomb=0; //시작점. 사용폭파 횟수 0 --> 1번 사용가능
 	Q.push(s);
-	visit[s.x][s.y][s.bomb]=1;
+	visit[s.x][s.y][0]=1;
+	visit[s.x][s.y][1]=1; // **시작점은 폭탄쓰든안쓰든 1
+
 	dist[s.x][s.y]=1;
-	dist[N-1][M-1]=-1;
+	//dist[N-1][M-1]=-1; // **안되는 경우에 출력하려고 이렇게 초기화 해두고 하면 문제생김
 
 	while(!Q.empty()){
-		int Q_size = Q.size();
-		while(Q_size--){
-			auto cur = Q.front(); Q.pop();
+		auto cur = Q.front(); Q.pop();
 
-			if(cur.x == N-1 and cur.y ==M-1){ // 목표도달
-				cout << "here!" << '\n';
-				cout << dist[N-1][M-1] << '\n';
-				return 0;
-			}
-			int nbomb = cur.bomb;
+		if(cur.x == N-1 and cur.y ==M-1){ // 목표도달
+			cout << dist[N-1][M-1] << '\n';
+			return 0;
+		}
+		int nbomb = cur.bomb;
 
+		for(int dir=0; dir<4; dir++){
+			int nx = cur.x + dx[dir];
+			int ny = cur.y + dy[dir];
+			
+			if(nx<0 or nx>=N or ny<0 or ny>=M) continue;
+			if(visit[nx][ny][nbomb]==1 or board[nx][ny]!='0') continue;
+
+			pos nxt;
+			nxt.x = nx; nxt.y = ny; nxt.bomb=nbomb;
+			
+			Q.push(nxt);
+			visit[nx][ny][nbomb]=1;
+			dist[nx][ny] = dist[cur.x][cur.y]+1;
+		}
+
+		if(nbomb==0){ // 아직 bomb 미사용?
 			for(int dir=0; dir<4; dir++){
 				int nx = cur.x + dx[dir];
 				int ny = cur.y + dy[dir];
-				cout << "nx, ny : " << nx << ' ' << ny <<'\n';
-				if(nx<0 or nx>=N or ny<0 or ny>=M) continue;
-				if(visit[nx][ny][nbomb]==1 or board[nx][ny]!='0') continue;
-
-				cout << "push! nx, ny : " << nx << ' ' << ny <<'\n'; 
+				//cout << "nx, ny : " << nx << ' ' << ny <<'\n';
+				if(nx<0 or nx >=N or ny <0 or ny >=M) continue;
+				if(visit[nx][ny][nbomb]==1 or board[nx][ny]=='0') continue;
 				pos nxt;
-				nxt.x = nx; nxt.y = ny; nxt.bomb=nbomb;
-				cout << "no bomb:"<< nx << ' '<< ny << '\n';
+				nxt.x = nx; nxt.y = ny; nxt.bomb=1; // 사용. 이제 사용 불가 
 				Q.push(nxt);
-				visit[nx][ny][nbomb]=1;
+				visit[nx][ny][nbomb]=1; // 사용된 상태로는 사용된 상태로서의 bfs가 전개 됨. 
 				dist[nx][ny] = dist[cur.x][cur.y]+1;
-			}
-
-			if(nbomb==0){ // 아직 bomb 미사용?
-				for(int dir=0; dir<4; dir++){
-					int nx = cur.x + dx[dir];
-					int ny = cur.y + dy[dir];
-					cout << "nx, ny : " << nx << ' ' << ny <<'\n';
-					if(nx<0 or nx >=N or ny <0 or ny >=N) continue;
-					if(visit[nx][ny][nbomb]==1 or board[nx][ny]=='0') continue;
-					pos nxt;
-					nxt.x = nx; nxt.y = ny; nxt.bomb=1; // 사용. 이제 사용 불가 
-					cout << "!!! bomb:"<< nx << ' '<< ny << '\n';
-					Q.push(nxt);
-					visit[nx][ny][nbomb]=1; // 사용된 상태로는 사용된 상태로서의 bfs가 전개 됨. 
-					dist[nx][ny] = dist[cur.x][cur.y]+1;
-				}
 			}
 		}
 	}	
-	cout << dist[N-1][M-1] << '\n';
+
+	cout << "-1" << '\n';
 	return 0;
 }
