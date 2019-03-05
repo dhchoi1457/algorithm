880279943ca6118ba7d0177691bd4f5b75418ca4
dhchoi1457 <alvin1785@gmail.com>
2019-03-06 00:16:32 +0900

    [boj6593]상범빌딩_BFS

--- boj6593.cpp
+++ boj6593.cpp
@@ -4,17 +4,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define X first
+#define Y second
+
 int L,R,C;
 
-string board[32][32]; // L R
-
+string board[32][32]; // L R C 
+int dist[32][32][32];
 //3-dim movement
-int dx[6]={1,0,0,-1,0,0};
-int dy[6]={0,1,0,0,-1,0};
-int dz[6]={0,0,1,0,0,-1};
+int dl[6]={1,0,0,-1,0,0};
+int dr[6]={0,1,0,0,-1,0};
+int dc[6]={0,0,1,0,0,-1};
 
-int sl, sr, sc;
-int el, er, ec;
+int sl, sr, sc; // start
+int el, er, ec; // end
 
 int main()
 {
@@ -27,6 +30,7 @@ int main()
 	// R : 한 층의 row
 	// C : 한 층의 column
 	while(L!=0 and R!=0 and C!=0){
+		int isTrap = 1;
 
 		for(int i=0; i<L; i++){
 			for(int j=0; j<R; j++){
@@ -36,14 +40,43 @@ int main()
 					if(board[i][j][k]=='S'){sl=i; sr=j; sc=k;}
 					if(board[i][j][k]=='E'){el=i; er=j; ec=k;}
 				}
+				fill(dist[i][j],dist[i][j]+C,-1);
 			}	
 		}
+		
+		queue<pair<int,pair<int,int>>> Q;
+		Q.push({sl,{sr,sc}});
+		dist[sl][sr][sc]=0;
+		while(!Q.empty()){
+			auto cur = Q.front(); Q.pop();
 
-		cout << sl << ' ' << sr << ' ' << sc <<'\n';
-		cout << el << ' ' << er << ' ' << ec <<'\n';
+			//cout << "cur : " << cur.X << ' ' << cur.Y.X << ' ' << cur.Y.Y <<'\n';
+
+			if(cur.X == el and cur.Y.X == er and cur.Y.Y == ec ){
+				
+				cout << "Escaped in "<< dist[el][er][ec] << " minute(s)." <<'\n';
+				isTrap = 0;
+				break;
+			}
+
+			for(int dir=0; dir<6; dir++){
+				int nl = cur.X + dl[dir];
+				int nr = cur.Y.X + dr[dir];
+				int nc = cur.Y.Y + dc[dir];
+
+				if(nl <0 or nl >= L or nr <0 or nr >= R or nc <0 or nc >=C) continue;
+				if(dist[nl][nr][nc]>=0 or board[nl][nr][nc]=='#') continue;
+				Q.push({nl,{nr,nc}});
+				dist[nl][nr][nc]= dist[cur.X][cur.Y.X][cur.Y.Y]+1;
+			}
+		}
+
+		if(isTrap==1){
+			cout << "Trapped!" << '\n';
+		}
+
+		cin >> L >> R >> C; 
 	}
 
-
-	cout << "use the stairs" << '\n';
 	return 0;
 } 
